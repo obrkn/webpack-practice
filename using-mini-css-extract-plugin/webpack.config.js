3f4cf454365507eca1506703523009c1dc93dbf9
obr-note <k33mdukb9n@yahoo.co.jp>
2022-02-15 08:31:48 +0900

    using-mini-css-extract-plugin

---
+++ using-mini-css-extract-plugin/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'js/bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/style.css',
+    }),
+  ],
+};
